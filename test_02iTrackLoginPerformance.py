807fffa77010b0c970a3772074151f259ddff1c1
Ednardo Xavier <128086546+ednardoxavier@users.noreply.github.com>
2025-06-18 09:45:00 -0300

    Update test_02iTrackLoginPerformance.py

--- test_02iTrackLoginPerformance.py
+++ test_02iTrackLoginPerformance.py
@@ -10,37 +10,36 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-class Test02iTrackLoginPerformance():
-      def setup_method(self, method):
-    self.driver = webdriver.Chrome()
-    self.vars = {}
-  
-  def teardown_method(self, method):
-    self.driver.quit()
-  
-  def test_02iTrackLoginPerformance(self):
-    self.driver.get("https://reman.esparta.io/acesso?callbackUrl=https%3A%2F%2Freman.esparta.io%2F")
-    self.driver.set_window_size(1722, 1034)
-    element = self.driver.find_element(By.ID, "drawer-settings-link")
-    actions = ActionChains(self.driver)
-    actions.move_to_element(element).perform()
-    element = self.driver.find_element(By.CSS_SELECTOR, "body")
-    actions = ActionChains(self.driver)
-    actions.move_to_element(element, 0, 0).perform()
-    element = self.driver.find_element(By.CSS_SELECTOR, ".mui-9wjka2")
-    actions = ActionChains(self.driver)
-    actions.move_to_element(element).perform()
-    element = self.driver.find_element(By.CSS_SELECTOR, "body")
-    actions = ActionChains(self.driver)
-    actions.move_to_element(element, 0, 0).perform()
-    self.driver.find_element(By.CSS_SELECTOR, "#drawer-settings-link .MuiTypography-root").click()
-    self.driver.find_element(By.ID, "mui-p-86671-T-transporters").click()
-    element = self.driver.find_element(By.CSS_SELECTOR, "body")
-    actions = ActionChains(self.driver)
-    actions.move_to_element(element, 0, 0).perform()
-    self.driver.find_element(By.ID, "mui-p-86671-T-users").click()
-    self.driver.find_element(By.ID, "mui-p-86671-T-receivers").click()
-    self.driver.find_element(By.ID, "mui-p-86671-T-docs").click()
-    self.driver.find_element(By.ID, "mui-p-86671-T-remans").click()
-    self.driver.find_element(By.CSS_SELECTOR, "#drawer-dashboard-link .MuiTypography-root").click()
-  
+class Test02iTrackLoginPerformance:
+    def setup_method(self, method):
+        self.driver = webdriver.Chrome()
+        self.vars = {}
+
+    def teardown_method(self, method):
+        self.driver.quit()
+
+    def test_02iTrackLoginPerformance(self):
+        self.driver.get("https://reman.esparta.io/acesso?callbackUrl=https%3A%2F%2Freman.esparta.io%2F")
+        self.driver.set_window_size(1722, 1034)
+        element = self.driver.find_element(By.ID, "drawer-settings-link")
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element).perform()
+        element = self.driver.find_element(By.CSS_SELECTOR, "body")
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element, 0, 0).perform()
+        element = self.driver.find_element(By.CSS_SELECTOR, ".mui-9wjka2")
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element).perform()
+        element = self.driver.find_element(By.CSS_SELECTOR, "body")
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element, 0, 0).perform()
+        self.driver.find_element(By.CSS_SELECTOR, "#drawer-settings-link .MuiTypography-root").click()
+        self.driver.find_element(By.ID, "mui-p-86671-T-transporters").click()
+        element = self.driver.find_element(By.CSS_SELECTOR, "body")
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element, 0, 0).perform()
+        self.driver.find_element(By.ID, "mui-p-86671-T-users").click()
+        self.driver.find_element(By.ID, "mui-p-86671-T-receivers").click()
+        self.driver.find_element(By.ID, "mui-p-86671-T-docs").click()
+        self.driver.find_element(By.ID, "mui-p-86671-T-remans").click()
+        self.driver.find_element(By.CSS_SELECTOR, "#drawer-dashboard-link .MuiTypography-root").click()
